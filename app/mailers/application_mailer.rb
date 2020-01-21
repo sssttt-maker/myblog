287b841d5141c6017ccf5565ef42f311938d597a
sssttt-maker <stst.shu82@gmail.com>
2020-01-21 21:25:20 +0900

    contact編集

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'shu-officialお問い合わせ'
   layout 'mailer'
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,6 +8,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: ENV['MAIL'], subject: "メールのタイトル"
+    mail to: ENV['MAIL'], subject: "お問い合わせ"
   end
 end
