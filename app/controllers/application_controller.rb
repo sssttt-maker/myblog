2a38cd634cf958cb1aff98e77ce91980e10f1eb2
sssttt-maker <stst.shu82@gmail.com>
2020-01-23 01:30:56 +0900

    herokuapp url redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_domain
+  FQDN = 'shu-engineer.com'
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :get_category
 
@@ -7,6 +9,14 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
