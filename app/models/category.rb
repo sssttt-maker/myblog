5e6abeb29f3d82685bac9c151b93ea7429c99b89
sssttt-maker <stst.shu82@gmail.com>
2020-01-24 02:31:35 +0900

    meta_tags修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :post_category_relations
+  has_many :post_category_relations, dependent: :destroy
   has_many :posts, through: :post_category_relations
-  has_many :gallery_category_relations
+  has_many :gallery_category_relations, dependent: :destroy
   has_many :galleries, through: :gallery_category_relations
 end
